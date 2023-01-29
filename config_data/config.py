4a8ca912fee0be8a7e3349d7ddeafa5e92867e56
KoVLya <ilakovunov@gmail.com>
2023-01-29 18:08:22 +0300

    config upd

---
+++ config_data/config.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class DatabaseConfig:
+    database: str         # Название базы данных
+    db_host: str          # URL-адрес базы данных
+    db_user: str          # Username пользователя базы данных
+    db_password: str      # Пароль к базе данных
+
+
+@dataclass
+class TgBot:
+    token: str            # Токен для доступа к телеграм-боту
+    admin_ids: list[int]  # Список id администраторов бота
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    db: DatabaseConfig
+
+env: Env = Env()
+env.read_env()
+
+config = Config(tg_bot=TgBot(token=env('BOT_TOKEN'),
+                             admin_ids=list(map(int, env.list('ADMIN_IDS')))),
+                db=DatabaseConfig(database=env('DATABASE'),
+                                  db_host=env('DB_HOST'),
+                                  db_user=env('DB_USER'),
+                                  db_password=env('DB_PASSWORD')))
+
+print('BOT_TOKEN', config.tg_bot.token)
+print('ADMIN_IDS', config.tg_bot.admin_ids, end='\n\n')
+print('DATABASE', config.db.database)
+print('DB_HOST', config.db.db_host)
+print('DB_USER', config.db.db_user)
+print('DB_PASSWORD', config.db.db_password)
