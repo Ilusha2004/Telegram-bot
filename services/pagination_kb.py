22f0fec031431669fc997e5f911184f78a24701d
KoVLya <ilakovunov@gmail.com>
2023-02-03 18:15:53 +0300

    new service feature upd

---
+++ services/pagination_kb.py
@@ -0,0 +1,13 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from lexicon.lexicon import LEXICON
+
+
+# Функция, генерирующая клавиатуру для страницы книги
+def create_pagination_keyboard(*buttons: str) -> InlineKeyboardMarkup:
+    # Создаем объект клавиатуры
+    pagination_kb: InlineKeyboardMarkup = InlineKeyboardMarkup()
+    # Наполняем клавиатуру кнопками
+    pagination_kb.row(*[InlineKeyboardButton(LEXICON[button] if button in LEXICON else button,
+                      callback_data=button) for button in buttons])
+    return pagination_kb
