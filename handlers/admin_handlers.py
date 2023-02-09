c26d421b0d019fef2a2d57af55d1f05a2bd7798e
KoVLya <ilakovunov@gmail.com>
2023-02-09 22:35:56 +0300

    wtf?!

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -2,8 +2,9 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 
 async def get_photo(message: Message):
-    with open("resources/image.jpeg", 'wr+') as file:
-        file = message.photo[0].file_id
+    with open("resources/image.jpeg", 'wr+') as file1:
+        file1 = message.photo[0].file_id
+        print(file1)
     await message.reply_photo(message.photo[0].file_id)
 
 # async def send_photo(message: Message):
